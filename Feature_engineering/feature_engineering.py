8138c881a5078e361cecf0c76a8cd833db2cd920
Yari Anrango <y.j.anrango@student.vu.nl>
2023-06-16 15:31:42 +0200

    idk

--- Feature_engineering/feature_engineering.py
+++ Feature_engineering/feature_engineering.py
@@ -8,9 +8,7 @@ dataset = pd.read_csv('/Users/yarianrango/Documents/School/Master-AI-VU/ML quant
 
 # Rolling functions
 print('rolling functions')
-dataset = rolling_functions(dataset, 100)
-
-
+# dataset = rolling_functions(dataset, 100)
 
 # Fourier Transformation
 print('FourierTransformation')
@@ -24,23 +22,23 @@ dataset = FreqAbs.abstract_frequency(dataset, ['value_heart'], ws, fs)
 dataset = FreqAbs.abstract_frequency(dataset, ['value_steps'], ws, fs)
 
 # One hot encoding
-dataset = dataset.drop(['start','end'], axis = 1)
-enc = OneHotEncoder()
-transformed_mat = enc.fit_transform(dataset[['value_workout']])
-transformed = pd.DataFrame.sparse.from_spmatrix(transformed_mat)
-transformed.columns = enc.categories_[0]
+# dataset = dataset.drop(['start','end'], axis = 1)
+# enc = OneHotEncoder()
+# transformed_mat = enc.fit_transform(dataset[['value_workout']])
+# transformed = pd.DataFrame.sparse.from_spmatrix(transformed_mat)
+# transformed.columns = enc.categories_[0]
+#
+# dataset = pd.concat([dataset, transformed], axis = 1)
+# dataset = dataset.drop('value_workout', axis = 1)
+# dataset[enc.categories_[0]] = dataset[enc.categories_[0]].sparse.to_dense()
 
-dataset = pd.concat([dataset, transformed], axis = 1)
-dataset = dataset.drop('value_workout', axis = 1)
-dataset[enc.categories_[0]] = dataset[enc.categories_[0]].sparse.to_dense()
 
-
-# Feature selection
+# #Feature selection
 # dataset = dataset.drop(['value_steps_max_freq', 'value_steps_freq_weighted','start','end'], axis = 1)
 # dataset = dataset.dropna()
 # print('Feature selection')
 # feat_sel = FeatureSelectionRegression()
-# X_train = dataset.loc[:,dataset.columns.drop(['temp_celsius','rain'])]
-# y_train = dataset.loc[:,['temp_celsius','rain']]
+# X_train = dataset.loc[:,dataset.columns.drop(['combined'])]
+# y_train = dataset.loc[:,['combined']]
 # selected_features, ordered_features, ordered_scores = feat_sel.forward_selection(100, X_train, y_train)
 
